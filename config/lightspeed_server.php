4fd2f4d9dae65c4433290857e80bbbdf4e0da36c
Wesley Peeters <Wesley@youngones.works>
2021-01-14 12:19:23 +0100

    Change port

--- config/lightspeed_server.php
+++ config/lightspeed_server.php
@@ -3,7 +3,7 @@
 declare(strict_types=1);
 
 return [
-    'port' => env('LIGHTSPEED_SERVER_PORT', 9811),
+    'port' => env('LIGHTSPEED_SERVER_PORT', 9810),
     'host' => env('LIGHTSPEED_SERVER_HOST', '127.0.0.1'),
     'log_level' => env('LIGHTSPEED_LOG_LEVEL', 'TRACE'),
 ];
